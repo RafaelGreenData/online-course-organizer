e230a928024975f21daca6dbf99e32c59ffe0e4c
Rafael Carvalho <87777432+RafaelGreenData@users.noreply.github.com>
2025-07-16 19:47:55 +0100

    Remove old project content

--- OpenCourseOrganizer/src/main/java/com/onlinecourseorganizer/repo/CourseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.onlinecourseorganizer.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.onlinecourseorganizer.model.Course;
-
-public interface CourseRepository extends JpaRepository<Course, Long> {
-    // Spring Data JPA gives you basic methods: save, findAll, findById, deleteById, etc.
-}
--- OpenCourseOrganizer/src/main/java/com/onlinecourseorganizer/repo/ModuleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.onlinecourseorganizer.repo;
-
-import com.onlinecourseorganizer.model.Module;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ModuleRepository extends JpaRepository<Module, Long> {
-}
